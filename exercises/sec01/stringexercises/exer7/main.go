496094ed13c2e07a90366f6a04697bb3564fcaef
Chris Nguyen <chrisng9@outlook.com>
2021-10-04 06:29:35 -0700

    Adding more exercises code to repo

---
+++ exercises/sec01/stringexercises/exer7/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Trim It
+//
+//  1. Look at the documentation of strings package
+//  2. Find a function that trims the spaces from
+//     the given string
+//  3. Trim the text variable and print it
+//
+// EXPECTED OUTPUT
+//  The weather looks good.
+//  I should go and play.
+// ---------------------------------------------------------
+
+func main() {
+	msg := `
+	
+	The weather looks good.
+I should go and play.
+	`
+
+	s := strings.Trim(msg, "\n\t")
+
+	s1 := strings.TrimSpace(msg)
+
+	fmt.Println(s)
+
+	fmt.Println(s1)
+}
