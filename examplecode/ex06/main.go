46e18818a421eabe376b51127c992beb4e59b953
Chris Nguyen <chrisng9@outlook.com>
2021-10-03 23:16:57 -0700

    Adding ex05 code to repo

--- examplecode/ex06/main.go
+++ examplecode/ex06/main.go
@@ -1,6 +1,33 @@
 // Convert value with type conversion
-package main 
+package main
+
+import "fmt"
 
 func main() {
+
+	// Short declaration when initial value is known
+	apple := 50
+	rice := 5
+	iceScream := 2
 	
+	applePrice := 1.20
+	ricePrice := 2.16
+	icePrice := 5.60
+
+	// Grouping variable for similar operation
+	var (
+		appleSum int
+		riceSum float64
+		iceScreamSum float64
+	)
+	
+	// type(value) to convert data type for math operation
+	appleSum = int(float64(apple) * applePrice)
+	fmt.Printf("Cost of apple : %T %v\n", appleSum, appleSum)
+
+	riceSum = (float64(rice) * ricePrice)
+	fmt.Printf("Cost of rice : %T %v\n", riceSum, riceSum)
+
+	iceScreamSum = float64(iceScream) * icePrice
+	fmt.Printf("Cost of iceScream : %T %v\n", iceScreamSum, iceScreamSum)
 }
