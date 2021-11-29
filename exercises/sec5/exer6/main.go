7bfc163aee07e2a8d7b66fbfa23544516a17f74c
Thanh Nguyen <chrisng9@outlook.com>
2021-11-29 02:15:03 -0800

    adding sec5 exercies

--- exercises/sec5/exer6/main.go
+++ exercises/sec5/exer6/main.go
@@ -7,45 +7,11 @@ import (
 )
 
 // ---------------------------------------------------------
-// EXERCISE: Housing Prices
+// EXERCISE: Housing Prices and Averages
 //
-//  We have received housing prices. Your task is to load the data into
-//  appropriately typed slices then print them.
+//  Use the previous exercise to solve this exercise (Housing Prices).
 //
-//  1. Check out the expected output
-//
-//
-//  2. Check out the code below
-//
-//     1. header   : stores the column headers
-//     2. data     : stores the real data related to each column
-//     3. separator: you will use it to separate the data
-//
-//
-//  3. Parse the data
-//
-//     1. Separate it into rows by using the newline character ("\n")
-//
-//     2. For each row, separate it by using the separator (",")
-//
-//
-//  4. Load the data into distinct slices
-//
-//     1. Load the locations into a []string
-//     2. Load the sizes into []int
-//     3. Load the beds into []int
-//     4. Load the baths into []int
-//     5. Load the prices into []int
-//
-//
-//  5. Print the header
-//
-//     1. Separate it by using the separator
-//
-//     2. Print each column 15 character wide ("%-15s")
-//
-//
-//  6. Print the rows from the slices that you've created, line by line
+//  Your task is to print the averages of the sizes, beds, baths, and prices.
 //
 //
 // EXPECTED OUTPUT
@@ -57,10 +23,8 @@ import (
 //  Paris          200            4              3              400000
 //  Istanbul       500            10             5              1000000
 //
-//
-// HINTS
-//
-//  + strings.Split function can separate a string into a []string
+//  ===========================================================================
+//                 237.50         4.75           2.75           425000.00
 //
 // ---------------------------------------------------------
 
@@ -75,6 +39,8 @@ Istanbul,500,10,5,1000000`
 		separator = ","
 	)
 
+	// Solve this exercise by using your previous solution for
+	// the "Housing Prices" exercise.
 	head := strings.Split(header, separator)
 
 	rows := strings.Split(data, "\n")
@@ -128,4 +94,17 @@ Istanbul,500,10,5,1000000`
 	for i := range rows {
 		fmt.Printf("%-14s %-14d %-14d %-14d %d\n", locs[i], size[i], beds[i], baths[i], prices[i])
 	}
+
+	fmt.Printf("%s\n", strings.Repeat("-", 70))
+
+	var ts, tb, tba, tp int
+	for i := range rows {
+		ts += size[i]
+		tb += beds[i]
+		tba += baths[i]
+		tp += prices[i]
+	}
+	l := len(rows)
+	fmt.Printf("%-14s %-14d %-14d %-14d %d\n", "", ts/l, tb/l, tba/l, tp/l)
+
 }
