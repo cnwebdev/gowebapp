2d181cf1c369f51ea24a4d9a0c6ecdbe2b9cb0dc
Thanh Nguyen <chrisng9@outlook.com>
2021-11-28 23:29:03 -0800

    adding sec5 exer2

--- exercises/sec5/exer1/main.go
+++ exercises/sec5/exer1/main.go
@@ -23,12 +23,15 @@ func main() {
 	png, header := []byte{'P', 'N', 'G'}, []byte{}
 
 	// 2. append elements to header to make it equal with the png slice
-	header = append(header, 'P', 'N', 'G')
+	header = append(header, png...)
+	// header = append(header, 'A')
 
 	// 3. compare the slices using the bytes.Equal function
 	var ok string
 	if bytes.Equal(png, header) {
 		ok = "equal"
+	} else {
+		ok = "not equal"
 	}
 
 	// 4. print whether they're equal or not
